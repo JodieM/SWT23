e673716589cbc3450e2a77b2260791b6f2da949f
JodieM <jminers@gmail.com>
2023-02-25 22:05:12 +1100

    First Run Through

--- force-app/main/default/lwc/accountKeyDetails/accountKeyDetails.js
+++ force-app/main/default/lwc/accountKeyDetails/accountKeyDetails.js
@@ -1,10 +1,6 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from 'lwc'
 
 export default class RecordViewFormDynamicContact extends LightningElement {
-    // Flexipage provides recordId and objectApiName
-    @api recordId;
-    @api objectApiName;
-
-    //From https://github.com/trailheadapps/lwc-recipes/blob/main/force-app/main/default/lwc/recordViewFormDynamicContact/recordViewFormDynamicContact.js
-    //Also https://www.sfdcblogs.com/post/record-view-form-in-lwc
+  @api recordId
+  @api objectApiName
 }
--- force-app/main/default/lwc/contactKeyAcctDetails/contactKeyAcctDetails.js
+++ force-app/main/default/lwc/contactKeyAcctDetails/contactKeyAcctDetails.js
@@ -1,42 +1,47 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { LightningElement, api, wire } from 'lwc'
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi'
 
-import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
-import RATING_FIELD from '@salesforce/schema/Account.Rating';
-import SITE_FIELD from '@salesforce/schema/Account.Site';
-import TYPE_FIELD from '@salesforce/schema/Account.Type';
-import WEB_FIELD from '@salesforce/schema/Account.Website';
-import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
-import CONT_FIELD from '@salesforce/schema/Contact.AccountId';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
+import RATING_FIELD from '@salesforce/schema/Account.Rating'
+import SITE_FIELD from '@salesforce/schema/Account.Site'
+import TYPE_FIELD from '@salesforce/schema/Account.Type'
+import WEB_FIELD from '@salesforce/schema/Account.Website'
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue'
+import CONT_FIELD from '@salesforce/schema/Contact.AccountId'
 
-
-const ACCT_FIELDS = [CONT_FIELD];
+const ACCT_FIELDS = [CONT_FIELD]
 
 export default class RecordViewFormDynamicContact extends LightningElement {
-    // Flexipage provides recordId and objectApiName
-    @api recordId; //Contact
-    @api objectApiName;
-    @api flexipageRegionWidth;
-    @wire(getRecord, { recordId: '$recordId', fields: ACCT_FIELDS })
-    cont;
+  // Flexipage provides recordId and objectApiName
+  @api recordId //Contact
+  @api objectApiName
+  @api flexipageRegionWidth
+  @wire(getRecord, { recordId: '$recordId', fields: ACCT_FIELDS })
+  cont
 
-    get accId() {
-        return getFieldValue(this.cont.data, CONT_FIELD);
-    }
+  get accId () {
+    return getFieldValue(this.cont.data, CONT_FIELD)
+  }
 
-    accountFields = [INDUSTRY_FIELD, TYPE_FIELD, RATING_FIELD, SITE_FIELD, WEB_FIELD, REVENUE_FIELD];
-    contactFields = [CONT_FIELD];
+  accountFields = [
+    INDUSTRY_FIELD,
+    TYPE_FIELD,
+    RATING_FIELD,
+    SITE_FIELD,
+    WEB_FIELD,
+    REVENUE_FIELD
+  ]
+  contactFields = [CONT_FIELD]
 
-    get smallRegion() {
-        return 'SMALL' === this.flexipageRegionWidth;
-    }
+  get smallRegion () {
+    return 'SMALL' === this.flexipageRegionWidth
+  }
 
-    get mediumRegion() {
-        return 'MEDIUM' === this.flexipageRegionWidth;
-    }
+  get mediumRegion () {
+    return 'MEDIUM' === this.flexipageRegionWidth
+  }
 
-    get largeRegion() {
-        return 'LARGE' === this.flexipageRegionWidth;
-    }
-
+  get largeRegion () {
+    return 'LARGE' === this.flexipageRegionWidth
+  }
 }
